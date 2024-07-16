2e883bc15c9725653288aae43a5826334bb440fb
aravindsagarpa98 <aravindsagar.pa@sainsburys.co.uk>
2024-07-16 23:06:20 +0100

    modified html files

--- pythonapp/app.py
+++ pythonapp/app.py
@@ -1,31 +1,25 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/cat')
+@app.route('/success')
 def success():
-    return render_template('cat.html')
+    return 'You are correct'
 
-@app.route('/dog')
+@app.route('/failure')
 def failure():
-    return render_template('dog.html')
-
-@app.route('/wrong')
-def failure():
-    return render_template('wrong.html')
+    return 'You are wrong'
 
 @app.route('/login', methods = ['POST'])
 def login():
     if request.method == 'POST':
         value = request.form['nm']
-        if value == 'cat':
-            return redirect(url_for('cat'))
-        elif value == 'dog':
-            return redirect(url_for('dog'))
+        if value == 'no':
+            return redirect(url_for('success'))
         else:
-            return redirect(url_for('wrong'))
+            return redirect(url_for('failure'))
     else:
-        return redirect(url_for('wrong'))
+        return redirect(url_for('failure'))
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 5000, debug = True)
